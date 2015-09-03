5dfe03a01653dbe02d1edd78433c59db703fd1cd
Josh Birdwell <jbirdwell34@gmail.com>
2015-09-03 02:12:37 -0500

    update data.js

--- public/javascript/data.js
+++ public/javascript/data.js
@@ -1,21 +1,62 @@
 // Casper's File!!!!!!!!!!!
 var https = require('https');
+var Q = require('q');
 
-var options = {
-	host: 'api.coursera.org',
-	path: '/api/catalog.v1/courses',
+/**
+ * wrapper for http request object
+ * @param  {Object} requestOptions
+ * @return Promise Object
+ */
+exports.promisedRequest = function(requestOptions) {
+	//create a deferred object from Q
+	var deferred  = Q.defer();
+
+	var req = https.request(requestOptions, function(response) {
+		// https.get(options,function (res) {
+		//     res.on('data', function(chunk) {
+		// 		chunks.push(chunk);
+		// 	});
+		// 	res.on('end', function(chunk) {
+		// 		var result = Buffer.concat(chunks);
+		//         JSON.parse(result);
+		// 	});
+		//     res.on('error', function (e) {
+		//         console.error(e);
+		//     });
+		// });
+	  response.setEncoding('utf8');
+	  var responseData = '';
+
+	  response.on('data', function(data) {
+		responseData += data;
+	  });
+
+	  response.on('end', function() {
+		deferred.resolve(responseData);
+	  });
+
+	});
+
+	req.on('error', function(err) {
+	  deferred.reject(err);
+	});
+
+	req.end();
+	return deferred.promise;
 };
 
-exports.getCourses = https.get(options, function(res) {
-	chunks = [];
-	res.on('data', function(chunk) {
-		chunks.push(chunk);
-	})
-	res.on('end', function(chunk) {
-		var result = Buffer.concat(chunks);
-		return JSON.parse(result);
-	})
-	res.on('error', function(err) {
-		return 'error: ' + err.message;
-	})
-});
+// var courses,
+// options = {
+// 	host: 'api.coursera.org',
+// 	path: '/api/catalog.v1/sessions?startMonth:9',
+// 	method: 'GET'
+// };
+//
+// this.promisedRequest(options)
+// 	.then(function(data) {
+// 		courses = JSON.stringify(data);
+// 		console.log(courses);
+//
+// 	}, function(error) {
+// 		console.log(error);
+// });
