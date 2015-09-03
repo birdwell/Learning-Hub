99177d75883efcf50463d2770a7c8c91920f0ddf
Casper Shepard <Shepard@Caspers-MBP.ok.cox.net>
2015-09-02 23:59:19 -0500

    API settup

--- public/javascript/data.js
+++ public/javascript/data.js
@@ -1,1 +1,21 @@
 // Casper's File!!!!!!!!!!!
+var https = require('https');
+
+var options = {
+	host: 'api.coursera.org',
+	path: '/api/catalog.v1/courses',
+};
+
+exports.getCourses = https.get(options, function(res) {
+	chunks = [];
+	res.on('data', function(chunk) {
+		chunks.push(chunk);
+	})
+	res.on('end', function(chunk) {
+		var result = Buffer.concat(chunks);
+		return JSON.parse(result);
+	})
+	res.on('error', function(err) {
+		return 'error: ' + err.message;
+	})
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var data = require('../public/javascript/data.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Learning Hub' });
+  res.render('index', { title: 'Learning Hub' });	
+});
+
+router.get('/courses', function(req, res, next) {
+	res.render('index', data.req);
 });
 
 module.exports = router;
