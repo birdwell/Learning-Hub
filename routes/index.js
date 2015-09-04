b0ce3a1c782b36f2c76095384fc0984fdf3e41b0
Josh Birdwell <jbirdwell34@gmail.com>
2015-09-03 19:30:20 -0500

    add twitter login support

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,16 @@ var express = require('express');
 var https = require('https');
 var router = express.Router();
 var Q = require('q');
+var passport = require('passport');
 var data = require('../public/javascript/courses.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Learning Hub' });
+  res.render('index', {user: req.user});
 });
 
 router.get('/courses', function(req, res, next) {
-    res.render('courses',data);
+    res.render('courses', data);
 });
 
 module.exports = router;
