4dbb2abff58e07e899188b8aac14282817d93d79
CasperShepard <shepardcasper@gmail.com>
2015-09-03 22:05:29 -0500

    update http request to include promise

--- public/javascript/data.js
+++ public/javascript/data.js
@@ -1,62 +1,33 @@
-// Casper's File!!!!!!!!!!!
+// clean this up by replacing += with join
+
 var https = require('https');
+var url = require('url');
 var Q = require('q');
 
-/**
- * wrapper for http request object
- * @param  {Object} requestOptions
- * @return Promise Object
+/*
+ * Returns a promise that contains an api JSON value.
+ *
  */
-exports.promisedRequest = function(requestOptions) {
-	//create a deferred object from Q
-	var deferred  = Q.defer();
 
-	var req = https.request(requestOptions, function(response) {
-		// https.get(options,function (res) {
-		//     res.on('data', function(chunk) {
-		// 		chunks.push(chunk);
-		// 	});
-		// 	res.on('end', function(chunk) {
-		// 		var result = Buffer.concat(chunks);
-		//         JSON.parse(result);
-		// 	});
-		//     res.on('error', function (e) {
-		//         console.error(e);
-		//     });
-		// });
-	  response.setEncoding('utf8');
-	  var responseData = '';
+function getContent(site) {
+	var def = Q.defer();
+	var options = url.parse(site);
+	var response = '';
 
-	  response.on('data', function(data) {
-		responseData += data;
-	  });
+	var req = https.request(options, function (res) {
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			response += chunk;
+		});
 
-	  response.on('end', function() {
-		deferred.resolve(responseData);
-	  });
-
-	});
-
-	req.on('error', function(err) {
-	  deferred.reject(err);
+		res.on('end', function () {
+			var json = JSON.parse(response);
+			def.resolve(json);
+		});
 	});
 
 	req.end();
-	return deferred.promise;
+	return def.promise;
 };
 
-// var courses,
-// options = {
-// 	host: 'api.coursera.org',
-// 	path: '/api/catalog.v1/sessions?startMonth:9',
-// 	method: 'GET'
-// };
-//
-// this.promisedRequest(options)
-// 	.then(function(data) {
-// 		courses = JSON.stringify(data);
-// 		console.log(courses);
-//
-// 	}, function(error) {
-// 		console.log(error);
-// });
+exports.getContent = getContent;
